aa896172dd0c134358d3cd915624323d27eb7183
Gabe <gabriel.velasquez0116@gmail.com>
2023-09-13 22:04:22 -0700

    working on seeds and making department roles, still need to work on more roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,13 @@ VALUES  ('Sales'),
         ('Engineering'),
         ('Human Resources');
 
+INSERT INTO role (title,salary, department_id)
+VALUES  ("Sales Representative", 54000, 1),
+        ("Sales Lead", 54000, 1),
+        ("Accountant", 130000, 2),
+        ("Account Manager", 175000, 2),
+        ("Lawyer", 54000, 3),
+        ("Legal Team Lead", ,3)
+
+INSERT INTO employee (first_name, last_name, role_id, mananger_id)
+VALUES
