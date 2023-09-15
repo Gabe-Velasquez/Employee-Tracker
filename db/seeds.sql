c9b34b518d515ae88670cd0f0e248320d9fa1405
Gabe <gabriel.velasquez0116@gmail.com>
2023-09-15 10:33:45 -0700

    coming up with roster of employees in seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,10 +8,37 @@ VALUES  ('Sales'),
 INSERT INTO role (title,salary, department_id)
 VALUES  ("Sales Representative", 54000, 1),
         ("Sales Lead", 54000, 1),
+        -- Accounting Team
         ("Accountant", 130000, 2),
-        ("Account Manager", 175000, 2),
-        ("Lawyer", 54000, 3),
-        ("Legal Team Lead", ,3)
+        ("Account Manager", 275000, 2),
+        -- Law Team
+        ("Lawyer", 180000, 3),
+        ("Legal Team Lead", 225000,3),
+        -- Engineering Team
+        ("Software Developer", 125000, 4),
+        ("Quality Assurance Tester", 65000, 4),
+        ("Lead Developer", 180000, 4),
+        -- HR Team
+        ("Human Resources Manager", 110000, 5),
+        ("Human Resources Representative", 60000, 5)
+
 
 INSERT INTO employee (first_name, last_name, role_id, mananger_id)
-VALUES
+VALUES ("Jay", "Garrick", 2, Null),
+       ("Barry", "Allen", 1, 2),
+       ("Bart", "Allen", 1, 2),
+       ("Wally", "West", 1, 2),
+       -- Accounting Team
+       ("Oliver", "Queen", 3, 4),
+       ("Bruce", "Wayne", 4, Null), 
+       -- Law Team    
+       ("Richard", "Grayson", 5, 6),
+       ("Harvey", "Dent",6, Null),
+       -- Engineering Team
+       ("Jonathan", "Osterman",4, 4),
+       ("Ray", "Palmer",4, 4),
+       ("Victor", "Stone",4, 4),
+       ("Martin", "Stein",4, 4),
+       ("Tim", "Drake",4, 4),
+       ("Lucius", "Fox",4, 4),
+       -- HR Team   
